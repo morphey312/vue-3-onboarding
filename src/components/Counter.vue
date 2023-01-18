1417a598df091889d41d38bf775642bb90fbb8c0
morphey312 <312chernenkods@gmail.com>
2023-01-18 14:21:17 +0200

    Lesson 1

---
+++ src/components/Counter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="input-group input-group-sm mb-3">
+      <span class="input-group-text" id="inputGroup-sizing-sm">Small</span>
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" :value="count">
+    </div>
+    <span>{{count}}</span>
+    <div>
+      <button @click="increment" type="button" class="btn btn-success">+</button>
+      <button @click="decrement" type="button" class="btn btn-danger">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+  methods: {
+    increment() {
+      this.count++
+    },
+    decrement() {
+      this.count--
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
